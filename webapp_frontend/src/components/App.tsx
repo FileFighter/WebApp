6f8c398f8fcec4f80d2db2f0de1e23b7f4c02a37
Gimleux <theo-2008@t-online.de>
2020-10-16 08:06:04 +0200

    Minor changes on first api call example

--- webapp_frontend/src/components/App.tsx
+++ webapp_frontend/src/components/App.tsx
@@ -1,41 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, {ReactElement, useEffect, useState} from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import {callBackendHealth} from "../api";
 
-function App() {
-  const[test,setTest] = useState("test");
+function App():ReactElement {
+    const [backendLiveTime, setBackendLiveTime] = useState<number | string>("Init");
 
-  useEffect(() => {
-    Promise.all([callBackendHealth()])
-        .then(([backendHealthData]) => {
-          setTest(backendHealthData.uptimeInSeconds)
-        })
-  }, [test]);
+    useEffect(() => {
+        updateVariables()
+    });
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={() => setTest(callBackendHealth().data)}>Test</button>
-        <p>{test}</p>
-      </header>
-    </div>
-  );
+    function updateVariables(): void {
+        Promise.all([callBackendHealth()])
+            .then(([backendHealthData]) => {
+                setBackendLiveTime(backendHealthData.uptimeInSeconds)
+            })
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Hello World
+                </p>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <button style={{marginTop: "20px"}} onClick={() => updateVariables()}>Test</button>
+                <p>{backendLiveTime}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
