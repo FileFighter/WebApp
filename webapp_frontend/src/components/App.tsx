532491cad427fa85acb6d99a60e957d0cffa8c75
qvalentin <valentin.theodor@web.de>
2020-11-17 14:15:08 +0100

    working redux exaple

--- webapp_frontend/src/components/App.tsx
+++ webapp_frontend/src/components/App.tsx
@@ -1,17 +1,18 @@
 import React, {ReactElement, useEffect, useState} from 'react';
 import { connect, ConnectedProps } from 'react-redux'
-import {addRefreshToken} from "../background/redux/actions";
+import {addAccessToken, addRefreshToken} from "../background/redux/actions";
 import {SystemState} from "../background/redux/actionTypes";
+import {Button} from "react-bootstrap";
 
 
 
 
 const mapState = (state: SystemState) => ({
-    tokens:{refreshToken:null,accessToken:null}
+    tokens:{refreshToken:state.tokens.refreshToken,accessToken:state.tokens.accessToken}
 })
 
 const mapDispatch = {
-    addRefreshToken
+    addRefreshToken,addAccessToken
 }
 
 const connector = connect(mapState, mapDispatch)
@@ -19,15 +20,20 @@ const connector = connect(mapState, mapDispatch)
 type PropsFromRedux = ConnectedProps<typeof connector>
 
 type Props = PropsFromRedux & {
-    backgroundColor: string
+
 }
 
-function App():ReactElement {
+function App(props: Props):ReactElement {
+
+
+
+    console.log(props.tokens.refreshToken)
+
 
 
     return (
         <div className="App">
-
+            <Button onClick={()=>props.addRefreshToken(12433)}>test</Button>
         </div>
     );
 }
