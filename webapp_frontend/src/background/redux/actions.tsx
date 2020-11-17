e4b14a75474432566875b25640f9decdd4a8d4bb
qvalentin <valentin.theodor@web.de>
2020-11-17 14:32:28 +0100

    remove one bug

--- webapp_frontend/src/background/redux/actions.tsx
+++ webapp_frontend/src/background/redux/actions.tsx
@@ -1,19 +1,13 @@
 import { ADD_REFRESH_TOKEN,ADD_ACCESS_TOKEN} from "./actionTypes";
 
 
-interface refreshToken{
-    refreshToken:number;
-}
+
 
 export const addRefreshToken = (content: number) => ({
     type: ADD_REFRESH_TOKEN,
-    payload: {
-        refreshToken: content
-    }
+    payload: content
 });
 export const addAccessToken = (content: number) => ({
     type: ADD_ACCESS_TOKEN,
-    payload: {
-        accessToken: content
-    }
+    payload: content
 });
--- webapp_frontend/src/components/App.tsx
+++ webapp_frontend/src/components/App.tsx
@@ -6,11 +6,12 @@ import {Button} from "react-bootstrap";
 
 
 
-
+// this takes the redux store and maps everything that is needed to the function props
 const mapState = (state: SystemState) => ({
     tokens:{refreshToken:state.tokens.refreshToken,accessToken:state.tokens.accessToken}
 })
 
+// this takes the redux actions and maps them to the props
 const mapDispatch = {
     addRefreshToken,addAccessToken
 }
@@ -19,6 +20,7 @@ const connector = connect(mapState, mapDispatch)
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
+// this defines the component props and also adds the redux imported props
 type Props = PropsFromRedux & {
 
 }
@@ -26,9 +28,11 @@ type Props = PropsFromRedux & {
 function App(props: Props):ReactElement {
 
 
-
+if (props.tokens.refreshToken) {
     console.log(props.tokens.refreshToken)
-
+    console.log(props.tokens)
+    console.log(1)
+}
 
 
     return (
