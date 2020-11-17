c15f1c2ddb28568a86674f7af786db5653ff6bbd
Gimleux <theo-2008@t-online.de>
2020-11-17 13:01:36 +0100

    Added sound helper

---
+++ webapp_frontend/src/components/basicElements/PermanentAssets.tsx
@@ -0,0 +1,9 @@
+import React, {ReactElement} from "react";
+//Assets
+import viking_ship from "../../assets/music/2017-04-01_-_Viking_Ship_-_David_Fesliyan.mp3"
+
+export default function PermanentAssets():ReactElement {
+    return (
+        <div></div>
+    )
+}
