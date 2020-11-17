ed98c2fd295da3c6bdf286beec94c3c0104dfb6c
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-17 11:42:50 +0100

    Added strings helper

---
+++ webapp_frontend/src/background/methods/strings.ts
@@ -0,0 +1,16 @@
+function reverseString(string:string):string {
+    if (string === "")
+        return "";
+    else
+        return reverseString(string.substr(1)) + string.charAt(0);
+}
+
+function stringReplaceSubstringOneTimeFromBeginningAndEnd(string:string, substring:string, replaceWith:string):string {
+    string = string.replace(substring, replaceWith);
+    string = reverseString(string);
+    string = string.replace(substring, replaceWith);
+    string = reverseString(string);
+    return string;
+}
+
+export {reverseString, stringReplaceSubstringOneTimeFromBeginningAndEnd}
