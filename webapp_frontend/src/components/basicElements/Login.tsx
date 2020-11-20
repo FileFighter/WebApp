ee85e9f72f2f25288fade182c19acd65e488c7e1
qvalentin <valentin.theodor@web.de>
2020-11-20 18:22:35 +0100

    added basic ui for login and hooked up checkbox

--- webapp_frontend/src/components/basicElements/Login.tsx
+++ webapp_frontend/src/components/basicElements/Login.tsx
@@ -1,6 +1,39 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useState} from 'react';
+import {Button, Form, Container} from "react-bootstrap";
 
-export default function Login():ReactElement {
-    return(<div>hallo</div>);
+
+type Props =  {}
+
+export default function Login(props: Props): ReactElement {
+    const [userName, SetUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [stayLoggedIn,setStayLoggedIn] =useState<boolean>(false);
+
+
+
+
+    return (
+        <Container>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"/>
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out"  onChange={()=>setStayLoggedIn(!stayLoggedIn)}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>);
 }
 
