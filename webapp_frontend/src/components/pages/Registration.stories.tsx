5ce294ef46a7afe429ed58be0e40b75374ec8ce4
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-22 00:21:46 +0100

    Added Registration UI

---
+++ webapp_frontend/src/components/pages/Registration.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {storiesOf} from "@storybook/react";
+import Registration from "./Registration";
+import {BrowserRouter} from "react-router-dom";
+import '../../style/custom.scss';
+
+storiesOf("Registration", module)
+    .add("default", () =>
+        <BrowserRouter>
+            <Registration/>
+        </BrowserRouter>
+    )
