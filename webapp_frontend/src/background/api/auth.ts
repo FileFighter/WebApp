9f5255ac2b2bbc7e597558ce20f803867337345b
qvalentin <valentin.theodor@web.de>
2020-11-21 18:46:47 +0100

    some layout

--- webapp_frontend/src/background/api/auth.ts
+++ webapp_frontend/src/background/api/auth.ts
@@ -36,6 +36,7 @@ export const loginWithUsernameAndPassword = (userName: string, password: string)
                 getAccessTokenWithRefreshToken()
             })
             .catch(((error) => {
+
                 reject(error);
             }))
 
