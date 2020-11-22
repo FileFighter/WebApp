29d4fb85e5aaffae3167b32969915dfd17b41335
open-schnick <jonathan.burst@gmx.de>
2020-11-22 16:36:36 +0100

    Added more properties to sonar-scanner.js

--- webapp_frontend/src/sonar-scanner.js
+++ webapp_frontend/src/sonar-scanner.js
@@ -1,13 +1,16 @@
 // sorry but this doesnt work in TypeScript pls dont kill me.
 const scanner = require('sonarqube-scanner');
- 
+
 scanner(
-  {
-    serverUrl : 'http://filefighter.ddns.net:9000',
-    options: {
-      'sonar.login': process.env.SONAR_LOGIN,
-      'sonar.password': process.env.SONAR_PASSWORD
-    }
-  },
-  () => process.exit()
+    {
+        serverUrl: 'http://filefighter.ddns.net:9000',
+        options: {
+            'sonar.projectKey': "de.filefighter.frontend",
+            'sonar.projectName': "Frontend",
+            'sonar.projectDescription': "Frontend for FileFighter",
+            'sonar.login': process.env.SONAR_LOGIN,
+            'sonar.password': process.env.SONAR_PASSWORD
+        }
+    },
+    () => process.exit()
 )
