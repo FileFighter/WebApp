fc1e939cf72ac4d336aa8adf4c67805176844595
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-23 16:09:29 +0100

    Deleted one douplicate in custom.scss

--- webapp_frontend/src/background/methods/strings.ts
+++ webapp_frontend/src/background/methods/strings.ts
@@ -1,11 +1,11 @@
-function reverseString(string:string):string {
+export function reverseString(string:string):string {
     if (string === "")
         return "";
     else
         return reverseString(string.substr(1)) + string.charAt(0);
 }
 
-function stringReplaceSubstringOneTimeFromBeginningAndEnd(string:string, substring:string, replaceWith:string):string {
+export function stringReplaceSubstringOneTimeFromBeginningAndEnd(string:string, substring:string, replaceWith:string):string {
     string = string.replace(substring, replaceWith);
     string = reverseString(string);
     string = string.replace(substring, replaceWith);
@@ -13,8 +13,6 @@ function stringReplaceSubstringOneTimeFromBeginningAndEnd(string:string, substri
     return string;
 }
 
-function deleteSpaces(string:string):string {
+export function deleteSpaces(string:string):string {
     return string.replace(/\s/,"")
 }
-
-export {reverseString, stringReplaceSubstringOneTimeFromBeginningAndEnd, deleteSpaces}
