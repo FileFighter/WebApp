9b9c9f7bcc4ffb69a91211769e4cce16c9a656d0
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-23 08:09:53 +0100

    Connected registration logic with ui

--- webapp_frontend/src/background/api/auth.ts
+++ webapp_frontend/src/background/api/auth.ts
@@ -8,8 +8,6 @@ import store from "../redux/store";
 import {addAccessToken, addRefreshToken} from "../redux/actions/tokens";
 import {addUser} from "../redux/actions/user";
 import {AccessToken, AddAccessToken, AddRefreshToken, TokensState} from "../redux/actions/tokenTypes";
-import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
 
 
 // reference: https://daveceddia.com/access-redux-store-outside-react/
@@ -17,10 +15,6 @@ import error = Simulate.error;
 export interface BackendLoginData {
     refreshToken: string,
     user: UserState
-
-}
-
-export interface BackendRegistrationData {
 
 }
 
@@ -48,37 +42,6 @@ export const loginWithUsernameAndPassword = (userName: string, password: string)
 
     })
 }
-
-export const registerNewUser = (username: string, password: string, passwordConfirmation: string): Promise<BackendRegistrationData> => {
-
-    let accessToken: AccessToken|null = (store.getState().tokens as TokensState).accessToken;
-
-    return new Promise<BackendRegistrationData>((resolve, reject) => {
-        let config = {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-            payload: {
-                username: username,
-                password: password,
-                confirmationPassword: passwordConfirmation
-            }
-        };
-
-        return Axios.get(hostname + username + "/register", config)
-            .then((data:AxiosResponse<object>) => {
-                console.log(data.status + ": " + data.statusText);
-                alert(data.status + ": " + data.statusText);
-            })
-            .catch((error:AxiosError) => {
-                //if(error.response!.status === 409){
-                    console.log("Error " + error.response!.status + ": " + error.response!.statusText);
-                //}
-                reject(error);
-            })
-    })
-}
-
 
 export const getAccessTokenWithRefreshToken = () => {
     console.log("getAccessTokenWithRefreshToken")
@@ -108,5 +71,3 @@ function setAuthHeaderToAxios(accessToken: string) {
     Axios.defaults.headers.common['Authorization'] =
         `Bearer ${accessToken}`;
 }
-
-
--- webapp_frontend/src/components/pages/Registration.tsx
+++ webapp_frontend/src/components/pages/Registration.tsx
@@ -1,10 +1,11 @@
 import React, {ChangeEvent, FormEvent, ReactElement, useState} from "react";
-import {Container, Row, Col, Form, FormGroup, Button} from "react-bootstrap";
+import {Container, Row, Col, Form, FormGroup, Button, Alert} from "react-bootstrap";
 import {deleteSpaces} from "../../background/methods/strings";
 import {notMinStrLength} from "../../background/methods/checkInput";
 import info_svg from "../../assets/images/icons/material.io/info-24px.svg";
 import check_svg from "../../assets/images/icons/material.io/check_circle-24px.svg";
 import error_svg from "../../assets/images/icons/material.io/error-24px.svg";
+import {registerNewUser} from "../../background/api/registration";
 
 export default function Registration(): ReactElement {
     const MIN_PASSWORD_LENGTH = 8;
@@ -17,12 +18,39 @@ export default function Registration(): ReactElement {
     const [passwordInformationUppercase, setPasswordInformationUppercase] = useState<boolean>(false);
     const [passwordInformationNumber, setPasswordInformationNumber] = useState<boolean>(false);
     const [passwordsMatch, setPasswordsMatch] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [alertMessage, setAlertMessage] = useState<string>("Error 404: No Message found.");
+    const [alertVariant, setAlertColor] = useState<"primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark">("success");
+    const [alertVisibility, setAlertVisibility] = useState<boolean>(false);
 
-    const handleSubmit = (event: FormEvent) => {
+    const handleSubmit = async (event: FormEvent) => {
+        console.log("[REGISTRATION] handleSubmit")
         event.preventDefault();
+        if (password !== passwordConfirmation) {
+            setAlertColor("danger");
+            setAlertMessage("Error: Password and password confirmation must match.")
+            handleAlertVisibility(3500)
+        } else {
+            await registerNewUser(username, password, passwordConfirmation)
+                .then(res => {
+                    setAlertMessage("Worked: " + res.user.username);
+                    setAlertColor("success");
+                    console.table(res);
+                    console.log(res.user)
+                })
+                .catch(err => {
+                    setAlertColor("danger");
+                    setAlertMessage("Some error occurred: " + err)
+                    console.log(err)
+                })
+                .finally(() => handleAlertVisibility(3500))
+        }
+    }
 
-        setErrorMessage("test");
+    const handleAlertVisibility = (duration: number) => {
+        setAlertVisibility(true);
+        setTimeout(() => {
+            setAlertVisibility(false)
+        }, duration)
     }
 
     const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -49,7 +77,7 @@ export default function Registration(): ReactElement {
             <Row>
                 <Col md={{span: 6, offset: 3}}>
                     <h1>Create new account</h1>
-                    <Form onSubmit={handleSubmit}>
+                    <Form onSubmit={() => handleSubmit}>
                         <FormGroup controlId="formBasicUsername">
                             <Form.Label>Username</Form.Label>
                             <Form.Control type={"name"} value={username}
@@ -62,22 +90,28 @@ export default function Registration(): ReactElement {
                                           value={password}
                                           onChange={(event: ChangeEvent<HTMLInputElement>) => handlePasswordChange(event)}/>
                             <div>
-                                <img alt={"status icon password length"} src={passwordInformationLength ? check_svg : info_svg}/>
+                                <img alt={"status icon password length"}
+                                     src={passwordInformationLength ? check_svg : info_svg}/>
                                 <span className={"sr-only"}>{passwordInformationLength ? "Done: " : "Missing: "}</span>
                                 <span className={passwordInformationLength ? "text-success" : "text-muted"}>Passwords must be at least 8 characters.</span>
                             </div>
                             <div>
-                                <img alt={"status icon password contains uppercase character"} src={passwordInformationUppercase ? check_svg : info_svg}/>
-                                <span className={"sr-only"}>{passwordInformationUppercase ? "Done: " : "Missing: "}</span>
+                                <img alt={"status icon password contains uppercase character"}
+                                     src={passwordInformationUppercase ? check_svg : info_svg}/>
+                                <span
+                                    className={"sr-only"}>{passwordInformationUppercase ? "Done: " : "Missing: "}</span>
                                 <span className={passwordInformationUppercase ? "text-success" : "text-muted"}>Passwords must be at least contain 1 uppercase character.</span>
                             </div>
                             <div>
-                                <img alt={"status icon password contains lowercase character"} src={passwordInformationLowercase ? check_svg : info_svg}/>
-                                <span className={"sr-only"}>{passwordInformationLowercase ? "Done: " : "Missing: "}</span>
+                                <img alt={"status icon password contains lowercase character"}
+                                     src={passwordInformationLowercase ? check_svg : info_svg}/>
+                                <span
+                                    className={"sr-only"}>{passwordInformationLowercase ? "Done: " : "Missing: "}</span>
                                 <span className={passwordInformationLowercase ? "text-success" : "text-muted"}>Passwords must be at least contain 1 lowercase character.</span>
                             </div>
                             <div>
-                                <img alt={"status icon password contains number"} src={passwordInformationNumber ? check_svg : info_svg}/>
+                                <img alt={"status icon password contains number"}
+                                     src={passwordInformationNumber ? check_svg : info_svg}/>
                                 <span className={"sr-only"}>{passwordInformationNumber ? "Done: " : "Missing: "}</span>
                                 <span className={passwordInformationNumber ? "text-success" : "text-muted"}>Passwords must be at least contain 1 number.</span>
                             </div>
@@ -88,15 +122,20 @@ export default function Registration(): ReactElement {
                                           value={passwordConfirmation}
                                           onChange={(event: ChangeEvent<HTMLInputElement>) => handlePasswordConfirmationChange(event)}/>
                             <div>
-                                <img alt={"status icon passwords match"} src={!passwordConfirmation ? info_svg : passwordsMatch ? check_svg : error_svg}/>
+                                <img alt={"status icon passwords match"}
+                                     src={!passwordConfirmation ? info_svg : passwordsMatch ? check_svg : error_svg}/>
                                 <span className={"sr-only"}>{passwordsMatch ? "Done: " : "Missing: "}</span>
-                                <span className={!passwordConfirmation ? "text-muted" : passwordsMatch ? "text-success" : "text-danger"}>Passwords must match.</span>
+                                <span
+                                    className={!passwordConfirmation ? "text-muted" : passwordsMatch ? "text-success" : "text-danger"}>Passwords must match.</span>
                             </div>
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Submit
                         </Button>
-                        <p className="text-danger">{errorMessage}</p>
+                        <Alert variant={alertVariant} onClose={() => setAlertVisibility(false)} show={alertVisibility}
+                               dismissible>
+                            <p>{alertMessage}</p>
+                        </Alert>
                     </Form>
                 </Col>
             </Row>
