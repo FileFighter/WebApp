4f4dcda0f6266f4cc3e23d55cb946a85b50ed9b4
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-23 15:58:37 +0100

    Fixed API registration problems

--- webapp_frontend/src/components/basicElements/Footer.tsx
+++ webapp_frontend/src/components/basicElements/Footer.tsx
@@ -1,13 +1,15 @@
 import React, {ReactElement} from 'react';
 
-export default function Footer():ReactElement {
-    return(
+export default function Footer(): ReactElement {
+    return (
         <div>
             <table>
+                <tbody>
                 <tr>
                     <td>Footer</td>
                     <td>Footer</td>
                 </tr>
+                </tbody>
             </table>
         </div>
     );
