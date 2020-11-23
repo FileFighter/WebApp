374b288611fd6f087f9b87ee7c278b538363eda6
Gimleux <theo-2008@t-online.de>
2020-11-23 16:00:16 +0100

    Fixed some warnings

--- webapp_frontend/src/components/pages/Registration.tsx
+++ webapp_frontend/src/components/pages/Registration.tsx
@@ -24,6 +24,7 @@ export default function Registration(): ReactElement {
 
     useEffect(() => {
         reviewPasswordMatch()
+        // eslint-disable-next-line
     },[passwordConfirmation, password])
 
     const handleSubmit = async (event: FormEvent) => {
