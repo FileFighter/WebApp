cbc1b38c9a2b86921113e0a4fd2596b43f2683c0
qvalentin <valentin.theodor@web.de>
2021-01-18 17:47:20 +0100

    fixed cors

--- webapp_frontend/src/background/constants.ts
+++ webapp_frontend/src/background/constants.ts
@@ -11,9 +11,9 @@ const prod: constantsdef = {
 
 const dev: constantsdef = {
     url: {
-       // API_URL: 'https://cors.filefighter.de/https://filefighter.de/api',
-        // API_URL: 'https://cors-anywhere.herokuapp.com/http://filefighter.ddns.net:3001/api',
-        API_URL: 'http://localhost:8080',
+        API_URL: 'https://cors.filefighter.de/https://filefighter.de/api',
+       // API_URL: 'http://localhost:8081/https://filefighter.de/api',
+        //API_URL: 'http://localhost:8080',
         //API_URL: 'https://filefighter.de/api',
     }
 };
