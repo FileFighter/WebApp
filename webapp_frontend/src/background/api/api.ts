d466a658480e1452b19930d2ae68285da1ecf27f
open-schnick <jonathan.burst@gmx.de>
2021-01-22 19:59:43 +0100

    renamed usedStorage

--- webapp_frontend/src/background/api/api.ts
+++ webapp_frontend/src/background/api/api.ts
@@ -1,37 +1,37 @@
-import Axios, { } from "axios";
+import Axios from "axios";
 import { constants } from "../constants";
 
 export const hostname: string = constants.url.API_URL;
 
-export const userPath: string = '/v1/users';
+export const userPath: string = "/v1/users";
 
-export const filesystemPath: string = '/v1/filesystem';
+export const filesystemPath: string = "/v1/filesystem";
 
 enum DataIntegrity {
-    STABLE = "systemHealth/traffic_green.png",
-    POSSIBLE_RISK = "systemHealth/traffic_yellow.png",
-    UNSTABLE = "systemHealth/traffic_red.png"
+  STABLE = "systemHealth/traffic_green.png",
+  POSSIBLE_RISK = "systemHealth/traffic_yellow.png",
+  UNSTABLE = "systemHealth/traffic_red.png",
 }
 
 interface SystemHealthData {
-    uptimeInSeconds: number;
-    userCount: number;
-    dataIntegrity: string;
-    deployment: string;
-    usedStorageInMb: number; // TODO: refactor this in RESTAPI.
-    version: string;
+  uptimeInSeconds: number;
+  userCount: number;
+  dataIntegrity: string;
+  deployment: string;
+  usedStorageInBytes: number;
+  version: string;
 }
 
 function callBackendHealth(): Promise<SystemHealthData> {
-    return new Promise((resolve, reject) => {
-        Axios.get(`${hostname}/health`)
-            .then((data) => {
-                resolve(data.data);
-            })
-            .catch(((error) => {
-                reject(error);
-            }));
-    });
+  return new Promise((resolve, reject) => {
+    Axios.get(`${hostname}/health`)
+      .then((data) => {
+        resolve(data.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 export { callBackendHealth, DataIntegrity };
--- webapp_frontend/src/components/pages/Health.tsx
+++ webapp_frontend/src/components/pages/Health.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/images/logos/logo.png";
 import { Button, Table, Container } from "react-bootstrap";
-import { callBackendHealth, DataIntegrity, SystemHealthData } from "../../background/api/api";
-import { audioOnOff, setAudioVolumeByID } from "../../background/methods/sound"
+import {
+  callBackendHealth,
+  DataIntegrity,
+  SystemHealthData,
+} from "../../background/api/api";
+import { audioOnOff, setAudioVolumeByID } from "../../background/methods/sound";
 import { logout } from "../../background/api/auth";
 import { getDurationAsString } from "../../background/methods/time";
 import { hasKey } from "../../background/methods/ObjectKeysTS";
@@ -10,105 +14,114 @@ import { formatBytes } from "../../background/methods/bytes";
 import { FFLoading } from "../../components/basicElements/Loading";
 
 export default function Health() {
+  const [systemHealthData, setSystemHealthData] = useState<
+    SystemHealthData | null | "loading"
+  >("loading");
+  const errorMsg = "not reachable";
 
-    const [systemHealthData, setSystemHealthData] = useState<SystemHealthData | null | "loading">("loading");
-    const errorMsg = "not reachable";
+  useEffect(() => {
+    updateVariables();
+  }, []);
 
-    useEffect(() => {
-        updateVariables();
-    }, [])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateVariables();
+    }, 60000);
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  });
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            updateVariables()
-        }, 60000)
-        // Clear timeout if the component is unmounted
-        return () => clearTimeout(timer);
-    });
+  function updateVariables(): void {
+    Promise.all([callBackendHealth()])
+      .then(([data]) => {
+        setSystemHealthData(data);
+      })
+      .catch(() => {
+        setSystemHealthData(null);
+      });
+  }
 
-    function updateVariables(): void {
-        Promise.all([callBackendHealth()])
-            .then(([data]) => {
-                setSystemHealthData(data)
-            })
-            .catch(() => {
-                setSystemHealthData(null)
-            })
+  function getPathOfDataIntegrity(dataIntegrity: string): string {
+    if (hasKey(DataIntegrity, dataIntegrity)) {
+      return DataIntegrity[dataIntegrity];
     }
+    console.log("[HEALTH] Couldn't parse SystemHealth string to enum.");
+    return errorMsg;
+  }
 
-    function getPathOfDataIntegrity(dataIntegrity: string): string {
-        if (hasKey(DataIntegrity, dataIntegrity)) {
-            return DataIntegrity[dataIntegrity]
-        }
-        console.log("[HEALTH] Couldn't parse SystemHealth string to enum.")
-        return errorMsg
+  function HealthContainer(): JSX.Element {
+    if (systemHealthData === null) {
+      return <p>{errorMsg}</p>;
     }
-
-    function HealthContainer(): JSX.Element {
-        if (systemHealthData === null) {
-            return <p>{errorMsg}</p>
-        } if (systemHealthData === "loading") {
-            return <FFLoading />
-        } else {
-            return (
-                <Container>
-                    <div>
-                        <Table striped bordered hover id={"ff-heath-table"}>
-                            <thead>
-                                <tr>
-                                    <th>System Health</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Deployment Type</td>
-                                    <td>{systemHealthData.deployment}</td>
-                                </tr>
-                                <tr>
-                                    <td>Version</td>
-                                    <td>{systemHealthData.version}</td>
-                                </tr>
-                                <tr>
-                                    <td>Uptime</td>
-                                    <td>{getDurationAsString(systemHealthData.uptimeInSeconds)}</td>
-                                </tr>
-                                <tr>
-                                    <td>Usercount</td>
-                                    <td>{systemHealthData.userCount}</td>
-                                </tr>
-                                <tr>
-                                    <td>Used Storage</td>
-                                    <td>{formatBytes(systemHealthData.usedStorageInMb)}</td>
-                                </tr>
-                                <tr>
-                                    <td>Data Integrity</td>
-                                    <td><img src={getPathOfDataIntegrity(systemHealthData.dataIntegrity)} alt={systemHealthData.dataIntegrity}></img></td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </div >
-                    <Button onClick={() => logout()}>Logout</Button>
-                </Container>
-            )
-        }
-    }
-
-    return (
+    if (systemHealthData === "loading") {
+      return <FFLoading />;
+    } else {
+      return (
         <Container>
-            <h1>
-                FileFighter
-            </h1>
-
-            <img
-                src={logo}
-                alt="Logo FileFighter"
-                onClick={() => {
-                    setAudioVolumeByID("audio_viking", 0.5)
-                    audioOnOff("audio_viking")
-                }}
-            />
-
-            <HealthContainer />
+          <div>
+            <Table striped bordered hover id={"ff-heath-table"}>
+              <thead>
+                <tr>
+                  <th>System Health</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Deployment Type</td>
+                  <td>{systemHealthData.deployment}</td>
+                </tr>
+                <tr>
+                  <td>Version</td>
+                  <td>{systemHealthData.version}</td>
+                </tr>
+                <tr>
+                  <td>Uptime</td>
+                  <td>
+                    {getDurationAsString(systemHealthData.uptimeInSeconds)}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Usercount</td>
+                  <td>{systemHealthData.userCount}</td>
+                </tr>
+                <tr>
+                  <td>Used Storage</td>
+                  <td>{formatBytes(systemHealthData.usedStorageInBytes)}</td>
+                </tr>
+                <tr>
+                  <td>Data Integrity</td>
+                  <td>
+                    <img
+                      src={getPathOfDataIntegrity(
+                        systemHealthData.dataIntegrity
+                      )}
+                      alt={systemHealthData.dataIntegrity}
+                    ></img>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <Button onClick={() => logout()}>Logout</Button>
         </Container>
-    )
+      );
+    }
+  }
+
+  return (
+    <Container>
+      <h1>FileFighter</h1>
+
+      <img
+        src={logo}
+        alt="Logo FileFighter"
+        onClick={() => {
+          setAudioVolumeByID("audio_viking", 0.5);
+          audioOnOff("audio_viking");
+        }}
+      />
+
+      <HealthContainer />
+    </Container>
+  );
 }
