f24fb5bfa39499861ad6273aeda79b56db2e2f69
qvalentin <valentin.theodor@web.de>
2021-01-23 10:24:46 +0100

    eslint here we go

--- webapp_frontend/cypress/integration/register_spec.js
+++ webapp_frontend/cypress/integration/register_spec.js
@@ -1,62 +1,64 @@
-import '../support/index'
+import '../support/index';
 
-const password="tghisZHdgaszugi342"
+const password = 'tghisZHdgaszugi342';
 
 describe('The register Page', () => {
+  beforeEach(() => {
+    cy.log('Login before every test');
+    cy.loginWithUrl('');
+  });
 
-    beforeEach(() => {
-        cy.log('Login before every test')
-        cy.loginWithUrl("")
-    })
+  it('registers a user successfully', () => {
+    cy.get('#ff-heath-table > tbody > tr:nth-child(4) > td:nth-child(2)').contains(/^\d+/).then(($tr) => {
+      const currentUserCount = $tr.text();
 
+      cy.log(`current Count of users: ${currentUserCount}`);
+      console.log(currentUserCount);
 
+      cy.get('span.navbar-link-description').contains('Registration').click();
 
-    it('registers a user successfully', () => {
+      const username = Math.floor(Math.random() * Math.floor(1000000)).toString();
+      cy.get('#registrationContainer #formBasicUsername').type(username);
+      cy.get('#formBasicPassword').type(password);
+      cy.get('#formConfirmPassword').type(password);
 
+      cy.get('.btn.btn-primary').contains('Submit').click();
+      cy.get('span.navbar-link-description').contains('Main').click();
 
+      cy.get('.btn.btn-primary').contains('Logout').click();
 
-        cy.get("#ff-heath-table > tbody > tr:nth-child(2) > td:nth-child(2)").contains(/^\d+/).then(($tr)=>
-        {
-            const currentUserCount = $tr.text()
+      cy.get('input[id=formBasicPassword]').type(`${password}{enter}`);
 
+      cy.get('h1').should('contain', 'FileFighter');
 
+      const newUserCount = (parseInt(currentUserCount) + 1).toString();
+      cy.log(newUserCount);
 
+      cy.log(`current Count of users: ${currentUserCount}`);
+      console.log(currentUserCount);
 
-        cy.log(`current Count of users: ${currentUserCount}`)
-        console.log(currentUserCount)
+      cy.get('span.navbar-link-description').contains('Registration').click();
 
-        cy.get("span.navbar-link-description").contains("Registration").click()
+      const username = Math.floor(Math.random() * Math.floor(1000000)).toString();
+      cy.get('#registrationContainer #formBasicUsername').type(username);
+      cy.get('#formBasicPassword').type(password);
+      cy.get('#formConfirmPassword').type(password);
 
+      cy.get('.btn.btn-primary').contains('Submit').click();
+      cy.get('span.navbar-link-description').contains('Main').click();
 
-        const username = Math.floor(Math.random() * Math.floor(1000000)).toString()
-        cy.get("#registrationContainer #formBasicUsername").type(username)
-        cy.get("#formBasicPassword").type(password)
-        cy.get("#formConfirmPassword").type(password)
+      cy.get('.btn.btn-primary').contains('Logout').click();
 
-        cy.get(".btn.btn-primary").contains("Submit").click()
-        cy.get("span.navbar-link-description").contains("Main").click()
+      cy.get('input[id=formBasicUsername]').type(username);
 
+      cy.get('input[id=formBasicPassword]').type(`${password}{enter}`);
 
+      cy.get('h1').should('contain', 'FileFighter');
 
-        cy.get(".btn.btn-primary").contains("Logout").click()
+      let newUserCount = (parseInt(currentUserCount) + 1).toString();
+      cy.log(newUserCount);
 
-
-        cy.get('input[id=formBasicUsername]').type(username)
-
-        cy.get('input[id=formBasicPassword]').type(`${password}{enter}`)
-
-
-        cy.get('h1').should('contain', 'FileFighter')
-
-
-            let newUserCount = (parseInt(currentUserCount)+1).toString();
-            cy.log(newUserCount);
-
-            cy.get("#ff-heath-table > tbody > tr:nth-child(2) > td:nth-child(2)").contains(/^\d+/).should('contain',newUserCount)
-        })
-    })
-
-
-
-
-})
+      cy.get('#ff-heath-table > tbody > tr:nth-child(4) > td:nth-child(2)').contains(/^\d+/).should('contain', newUserCount);
+    });
+  });
+});
