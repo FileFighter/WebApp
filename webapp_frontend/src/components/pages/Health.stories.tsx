7ea679faefb7eab381d1b0ec4c3e3b5011c39ea8
qvalentin <valentin.theodor@web.de>
2021-01-24 14:46:20 +0100

    fixed api call for story

--- webapp_frontend/src/components/pages/Health.stories.tsx
+++ webapp_frontend/src/components/pages/Health.stories.tsx
@@ -3,9 +3,21 @@ import { storiesOf } from "@storybook/react";
 import { BrowserRouter } from "react-router-dom";
 import "../../style/custom.scss";
 import Health from "./Health";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { hostname } from "../../background/api/api";
+import healthApiMock from "./__tests__/healthApiMock.json";
 
-storiesOf("Health", module).add("default", () => (
-  <BrowserRouter>
-    <Health />
-  </BrowserRouter>
-));
+const mock = new MockAdapter(axios);
+
+// 2.1 the api request to be intercepted has to match exactly
+const API_REQUEST = hostname + "/health";
+
+storiesOf("Health", module).add("default", () => {
+  mock.onGet(API_REQUEST).reply(200, healthApiMock);
+  return (
+    <BrowserRouter>
+      <Health />
+    </BrowserRouter>
+  );
+});
