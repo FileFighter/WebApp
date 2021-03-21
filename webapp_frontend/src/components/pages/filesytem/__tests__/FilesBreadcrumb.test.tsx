5042acada006f6f532a2becfba6e38bd15dc5126
valentin <36446499+qvalentin@users.noreply.github.com>
2021-03-21 10:16:34 +0100

    Feature/jest basics (#110)
    * added one test
    
    * run tests in pipeline
    
    * update snapshot
    
    * Update FilesBreadcrumb.tsx
    
    Added Location to console.log
    
    Co-authored-by: Gimleux <56401687+Gimleux@users.noreply.github.com>

---
+++ webapp_frontend/src/components/pages/filesytem/__tests__/FilesBreadcrumb.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { FilesBreadcrumb } from "../FilesBreadcrumb";
+import { RouterWrapper } from "../../../../dev/testUtils/RouterWrapper";
+
+test("renders path Breadcrumb", () => {
+  const breadcrumb = render(
+    <RouterWrapper>
+      <FilesBreadcrumb path={"/bla/fasel/file"} setPath={jest.fn()} />
+    </RouterWrapper>
+  );
+
+  expect(breadcrumb).toMatchSnapshot();
+});
