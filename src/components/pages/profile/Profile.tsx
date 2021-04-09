9871b6b56f9a43f5347d1d7a87f423faac310a7d
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-04-09 13:54:47 +0200

    show profile informations

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,15 +1,37 @@
-import React, {Props, ReactElement, useState} from "react";
-import {Container} from "react-bootstrap";
-import {genitiveOfWord} from "../../../background/methods/grammar/germanGrammar";
-import {SystemState} from "../../../background/redux/actions/sytemState";
+import React, {ReactElement} from "react";
+import {Container, Row, Button} from "react-bootstrap";
 
-export default function Profile() : ReactElement {
+export default function Profile(): ReactElement {
+    const user = {
+        name: "Gimleux",
+        groups: ["FAMILY", "ADMIN"]
+    };
 
     return (
-        <Container>
-            <h2>
-                Profile
-            </h2>
+        <Container className="page-content">
+            <Container fluid>
+                <Row className="title-action mt-1">
+                    <h1 className="mr-1 h4">
+                        My Profile
+                    </h1>
+                    <Button>Edit Informations</Button>
+                </Row>
+            </Container>
+
+            <Container className="profile-information-display p-0">
+                <h2 className="h3 pt-3 pb-3">
+                    {user.name}
+                </h2>
+                <dl>
+                    <dt>Username</dt>
+                    <dd>{user.name}</dd>
+                    <dt>Groups</dt>
+                    <dd>{user.groups.map((group: string) => {
+                        return group + " "
+                    })}
+                    </dd>
+                </dl>
+            </Container>
         </Container>
     );
 }
