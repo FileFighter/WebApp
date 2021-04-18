936aeccaece4c496bc871760228105b2bc5f464a
Gimleux <theo-2008@t-online.de>
2021-04-19 00:53:47 +0200

    Added background image

--- src/components/basicElements/Login.tsx
+++ src/components/basicElements/Login.tsx
@@ -1,5 +1,5 @@
 import React, {FormEvent, ReactElement, useState} from 'react';
-import {Button, Form, Container, Row,Col,Spinner, Image} from "react-bootstrap";
+import {Button, Col, Container, Form, Image, Row, Spinner} from "react-bootstrap";
 import {loginWithUsernameAndPassword} from "../../background/api/auth";
 
 import logo from "../../assets/images/logos/logoWithWhiteBorder.png";
@@ -9,13 +9,13 @@ function Login(): ReactElement {
     const [password, setPassword] = useState<string>("");
     const [stayLoggedIn, setStayLoggedIn] = useState<boolean>(true);
     const [errorMessage, setErrorMessage] = useState<string>("");
-    const [loading,setLoading]=useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
         setLoading(true)
-        loginWithUsernameAndPassword(userName, password,stayLoggedIn)
+        loginWithUsernameAndPassword(userName, password, stayLoggedIn)
             .then(() => {
                 //nothing to do here :)
                 setErrorMessage("");
@@ -32,55 +32,58 @@ function Login(): ReactElement {
 
 
     return (
-        <Container  className="h-100">
-            <Container fluid className="login-container">
+        <Container fluid className="h-100 ml-0 mr-0 login-page">
+            <Container fluid className="login-container bg-body-bg">
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Image rounded src={logo} height="200px" width="auto"/>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <h1>Greetings Traveller!</h1>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <h2>Be welcome at FileFighter</h2>
+                    </Row>
+                </Container>
+                <Container className="login-input">
+                    <Row className="mt-4">
+                        <Col>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="formBasicUsername">
+                                    <Form.Control placeholder="Username" value={userName}
+                                                  onChange={event => setUsername(event.target.value)}/>
+                                </Form.Group>
 
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Image rounded src={logo} height="200px" width="auto"/>
-                </Row>
-                <Row className="justify-content-md-center">
-                    <h1>Greetings Traveller!</h1>
-                </Row>
-                <Row className="justify-content-md-center">
-                    <h2>Be welcome at FileFighter</h2>
-                </Row>
-            </Container>
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Control type="password" placeholder="Password" value={password}
+                                                  onChange={event => setPassword(event.target.value)}/>
+                                    <Form.Text className="text-muted">
+                                        Contact your administrator if you have forgotten your login details.
+                                    </Form.Text>
+                                </Form.Group>
 
-            <Row className="mt-4">
-                <Col>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="formBasicUsername">
-                            <Form.Control placeholder="Username" value={userName} onChange={event => setUsername(event.target.value)}/>
-                        </Form.Group>
-
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)}/>
-                            <Form.Text className="text-muted">
-                                Contact your administrator if you have forgotten your login details.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit" block>
-                            <Spinner
-                                as="span"
-                                animation="grow"
-                                size="sm"
-                                role="status"
-                                aria-hidden="true"
-                                className={loading? "" :"d-none"}
-                            />   <span className={loading? "sr-only" :"d-none"}>Loading...</span>Sign in
-                        </Button>
-                        <Form.Group controlId="formBasicCheckbox" className="mt-3 justify-content-center">
-                            <Form.Check checked={stayLoggedIn} type="checkbox" label="Keep me signed in*" onChange={() => setStayLoggedIn(!stayLoggedIn)}/>
-                            <Form.Text className="text-muted">
-                                *By clicking this, you accept the usage of cookies.
-                            </Form.Text>
-                        </Form.Group>
-                        <p className="text-danger">{errorMessage}</p>
-                    </Form>
-                </Col>
-            </Row>
+                                <Button variant="primary" type="submit" block>
+                                    <Spinner
+                                        as="span"
+                                        animation="grow"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                        className={loading ? "" : "d-none"}
+                                    /> <span className={loading ? "sr-only" : "d-none"}>Loading...</span>Sign in
+                                </Button>
+                                <Form.Group controlId="formBasicCheckbox" className="mt-3 justify-content-center">
+                                    <Form.Check checked={stayLoggedIn} type="checkbox" label="Keep me signed in*"
+                                                onChange={() => setStayLoggedIn(!stayLoggedIn)}/>
+                                    <Form.Text className="text-muted">
+                                        *By clicking this, you accept the usage of cookies.
+                                    </Form.Text>
+                                </Form.Group>
+                                <p className="text-danger">{errorMessage}</p>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </Container>);
 }
