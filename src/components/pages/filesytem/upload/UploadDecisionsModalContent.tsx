eac09161b85a1f2657574bac382233db82404d70
qvalentin <valentin.theodor@web.de>
2021-05-13 17:09:42 +0200

    add action to overwrite/merge all button

--- src/components/pages/filesytem/upload/UploadDecisionsModalContent.tsx
+++ src/components/pages/filesytem/upload/UploadDecisionsModalContent.tsx
@@ -55,13 +55,20 @@ export const UploadDecisionsModalContent = ({
           setPreflightResultDispatch({ type: PREFLIGHT_ADD_ENTITIES, payload: combined });
         } else {
           uploadFiles(combined.filter(
-            (f: EditablePreflightEntityOrFile) => !f.isFile) as EditableFileWithPreflightInfo[], fsItemIdToUpload);
+            (f: EditablePreflightEntityOrFile) => f.isFile) as EditableFileWithPreflightInfo[], fsItemIdToUpload);
           handleClose();
           setPreflightResultDispatch({ type: PREFLIGHT_ADD_ENTITIES, payload: [] });
         }
         setLoading(false);
       });
   };
+
+  const handleOverWriteAll = () => {
+      uploadFiles(preflightResult.filter(
+          (f: EditablePreflightEntityOrFile) => f.isFile) as EditableFileWithPreflightInfo[], fsItemIdToUpload);
+      handleClose();
+      setPreflightResultDispatch({ type: PREFLIGHT_ADD_ENTITIES, payload: [] });
+  }
 
   const foldersToMerge = preflightResult.filter(
     (f: EditablePreflightEntityOrFile) => !f.isFile && f.nameAlreadyInUse
@@ -163,7 +170,7 @@ export const UploadDecisionsModalContent = ({
               </Button>
               <Button
                 variant="primary"
-                onClick={handleClose}
+                onClick={handleOverWriteAll}
                 disabled={
                   !!insufficientPermission.length ||
                   !!entitiesWithInvalidName.length
