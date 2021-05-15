22be102404cd03952d6892f92e8b32a6e3f37632
codefactor-io <support@codefactor.io>
2021-05-15 16:24:04 +0000

    [CodeFactor] Apply fixes to commit 61a4df8

--- src/background/methods/passwords.ts
+++ src/background/methods/passwords.ts
@@ -1,4 +1,4 @@
-//see: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest 
+//see: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 async function hashPassword(password: string) {
     const msgUint8 = new TextEncoder().encode(password);                           // encode as (utf-8) Uint8Array
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
