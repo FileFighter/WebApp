58777d5d25af8ed90615bf3e6a8882707ebd8e29
qvalentin <valentin.theodor@web.de>
2021-05-17 08:54:58 +0200

    make hashed password HEX code uppercase

--- src/background/methods/passwords.ts
+++ src/background/methods/passwords.ts
@@ -5,7 +5,7 @@ async function hashPassword(password: string) {
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
     const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-    return hashHex;
+    return hashHex.toUpperCase();
 }
 
 export {hashPassword}
