c374852fac9ffd24ffcda6a5c13d958b06d69cf6
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-17 16:35:56 +0200

    WIP Context Menu Content

--- src/components/pages/filesytem/FileItemContextMenu.tsx
+++ src/components/pages/filesytem/FileItemContextMenu.tsx
@@ -5,6 +5,21 @@ interface FileItemContextMenuInterface {
     id: string|number
 }
 
+interface DropdownItemTitleInterface {
+    icon?: string;
+    description: string
+}
+
+const DropdownItemTitle = (props:DropdownItemTitleInterface) => {
+    const {icon, description} = props;
+    return(
+        <span className="d-flex w-100">
+            <span className="flex-grow-0 pr-2 w-75">{icon}</span>
+            <span className="flex-grow-1"> {description}</span>
+        </span>
+    )
+}
+
 function FileItemContextMenu(props:FileItemContextMenuInterface) {
     const {id} = props;
     return(
@@ -14,9 +29,11 @@ function FileItemContextMenu(props:FileItemContextMenuInterface) {
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item href="#/action-1"><DropdownItemTitle icon={"&#128393;"} description={"Rename"}/></Dropdown.Item>
+                <Dropdown.Item href="#/action-2">&#128190; Download</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">&#9959; Details</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-4">&#10551; Share</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
