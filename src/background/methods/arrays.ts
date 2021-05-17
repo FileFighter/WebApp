9940e5dbd208f1da9e97471aed6850ec493c1d4b
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-17 14:55:14 +0200

    removed else in arrays.ts

--- src/background/methods/arrays.ts
+++ src/background/methods/arrays.ts
@@ -1,16 +1,15 @@
 export function divideArrayByCondition<T>(
-  input: T[],
-  condition: (a: T) => boolean
+    input: T[],
+    condition: (a: T) => boolean
 ) {
-  let output: T[][] = [[], []];
+    let output: T[][] = [[], []];
 
-  input.forEach((el: T) => {
-    if (condition(el)) {
-      output[0].push(el);
-    } else {
-      output[1].push(el);
-    }
-  });
+    input.forEach((el: T) => {
+        if (!condition(el)) {
+            return output[1].push(el);
+        }
+        output[0].push(el);
+    });
 
-  return output;
+    return output;
 }
