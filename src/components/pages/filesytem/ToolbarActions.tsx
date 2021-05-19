ed394bdbea02c0c73ba7c1681ff21cf90f9ab5df
qvalentin <valentin.theodor@web.de>
2021-05-19 17:25:15 +0200

    add delete to context menu

--- src/components/pages/filesytem/ToolbarActions.tsx
+++ src/components/pages/filesytem/ToolbarActions.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Button, Fade } from "react-bootstrap";
+import { Button, Dropdown, Fade } from "react-bootstrap";
 import { SystemState } from "../../../background/redux/actions/sytemState";
 import { connect, ConnectedProps } from "react-redux";
 import { deleteFsEntities } from "../../../background/api/filesystem";
@@ -33,11 +33,7 @@ function ToolbarActions(props: Props): ReactElement | null {
       <Fade in={props.selectedFsEntities.length > 0}>
         <span>
           <Button onClick={handleDeleteClicked} disabled={props.selectedFsEntities.length < 1}>Delete</Button>
-            <form method="get" className="d-inline"
-                  action={constants.url.FH_URL + "/download"}>
-               <input className="d-none" type='text' name='ids' value={props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())}/>
-            <Button  type="submit" disabled={props.selectedFsEntities.length < 1}>Download</Button>
-        </form>
+            <Button disabled={props.selectedFsEntities.length < 1}  href={constants.url.FH_URL + "/download?ids=" + props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())} >Download</Button>
         </span>
       </Fade>
       <Button>New Folder</Button>
