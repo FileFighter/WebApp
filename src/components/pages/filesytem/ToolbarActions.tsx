95e8e99c9bbaea4de10eff1219063931750cbd62
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-19 17:43:21 +0200

    Delete unused imports

--- src/components/pages/filesytem/ToolbarActions.tsx
+++ src/components/pages/filesytem/ToolbarActions.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Button, Dropdown, Fade } from "react-bootstrap";
+import { Button, Fade } from "react-bootstrap";
 import { SystemState } from "../../../background/redux/actions/sytemState";
 import { connect, ConnectedProps } from "react-redux";
 import { deleteFsEntities } from "../../../background/api/filesystem";
@@ -33,7 +33,8 @@ function ToolbarActions(props: Props): ReactElement | null {
       <Fade in={props.selectedFsEntities.length > 0}>
         <span>
           <Button onClick={handleDeleteClicked} disabled={props.selectedFsEntities.length < 1}>Delete</Button>
-            <Button disabled={props.selectedFsEntities.length < 1}  href={constants.url.FH_URL + "/download?ids=" + props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())} >Download</Button>
+            <Button disabled={props.selectedFsEntities.length < 1}
+                    href={constants.url.FH_URL + "/download?ids=" + props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())}>Download</Button>
         </span>
       </Fade>
       <Button>New Folder</Button>
