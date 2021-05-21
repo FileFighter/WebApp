71c4aeb4638c717cac96652899395fad880b5be7
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-21 14:42:41 +0200

    resize Footer

--- src/components/basicElements/Footer.tsx
+++ src/components/basicElements/Footer.tsx
@@ -1,19 +1,16 @@
-import React, {ReactElement} from 'react';
-import {Container} from 'react-bootstrap';
-import github from "super-tiny-icons/images/svg/github.svg"
+import React, { ReactElement } from "react";
+import { Container } from "react-bootstrap";
 
 export default function Footer(): ReactElement {
     return (
-        <footer className='footer mt-auto py-3 text-white'>
-            <Container className="d-flex flex-column justify-content-center bg-primary p-1">
-                <a href={"https://github.com/filefighter"} className="align-self-center"><img src={github}
-                                                                                              alt={"github logo"}
-                                                                                              width={"50px"}/></a>
-                <span className="align-self-center mt-2">Made by</span>
+        <footer className="footer mt-autotext-white bg-primary">
+            <Container className="d-flex flex-column justify-content-center p-0">
+                <span className="align-self-center mt-2"><a href={"https://github.com/filefighter"}
+                                                            className="text-white">FileFighter</a> is made with &lt;3 by</span>
                 <div className="d-flex justify-content-around">
-                    <span>Gimleux</span>
-                    <span>Open-Schnick</span>
-                    <span>qvalentin</span>
+                    <span><a href="https://github.com/Gimleux" className="text-white">Gimleux</a></span>
+                    <span><a href="https://github.com/open-schnick" className="text-white">Open-Schnick</a></span>
+                    <span><a href="https://github.com/qvalentin" className="text-white">qvalentin</a></span>
                 </div>
             </Container>
         </footer>
