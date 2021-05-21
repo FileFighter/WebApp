b515e99f601fea12e598c843d4c869d1c1bacc45
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-21 14:47:19 +0200

    reinclude github logo in footer

--- src/components/basicElements/Footer.tsx
+++ src/components/basicElements/Footer.tsx
@@ -1,12 +1,19 @@
 import React, { ReactElement } from "react";
 import { Container } from "react-bootstrap";
+import github from "super-tiny-icons/images/svg/github.svg"
 
 export default function Footer(): ReactElement {
     return (
         <footer className="footer mt-autotext-white bg-primary">
             <Container className="d-flex flex-column justify-content-center p-0">
-                <span className="align-self-center mt-2"><a href={"https://github.com/filefighter"}
-                                                            className="text-white">FileFighter</a> is made with &lt;3 by</span>
+                <span className="align-self-center mt-2">
+                    <a href={"https://github.com/filefighter"} className="text-white">
+                        FileFighter&#160;
+                        <img src={github}
+                             alt={"github logo"}
+                             height="16px"/>
+                    </a> is made with &lt;3 by
+                </span>
                 <div className="d-flex justify-content-around">
                     <span><a href="https://github.com/Gimleux" className="text-white">Gimleux</a></span>
                     <span><a href="https://github.com/open-schnick" className="text-white">Open-Schnick</a></span>
