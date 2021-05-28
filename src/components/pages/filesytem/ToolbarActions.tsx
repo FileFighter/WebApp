1734d9f6ede3699cdb46b651b0fb9d426ea75136
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-28 11:43:15 +0200

    changed toolbar button design

--- src/components/pages/filesytem/ToolbarActions.tsx
+++ src/components/pages/filesytem/ToolbarActions.tsx
@@ -25,31 +25,31 @@ function ToolbarActions(props: Props): ReactElement | null {
 
     return (
         <span>
-      <Fade in={props.selectedFsEntities.length === 1}>
-        <Button
-            size="sm"
-            variant="outline-secondary"
-            disabled={props.selectedFsEntities.length !== 1}
-        >Rename</Button>
-      </Fade>
-      <Fade in={props.selectedFsEntities.length > 0}>
-        <span>
-          <Button
-              size="sm"
-              variant="outline-secondary"
-              onClick={handleDeleteClicked}
-              disabled={props.selectedFsEntities.length < 1}
-              className="mx-1"
-          >Delete</Button>
-            <Button
-                size="sm"
-                variant="outline-secondary"
-                disabled={props.selectedFsEntities.length < 1}
-                href={constants.url.FH_URL + "/download?ids=" + props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())}
-            >Download</Button>
-        </span>
-      </Fade>
-      <NewFolder />
+            <Fade in={props.selectedFsEntities.length === 1}>
+                <Button
+                    size="sm"
+                    variant="outline-secondary"
+                    disabled={props.selectedFsEntities.length !== 1}
+                >Rename</Button>
+            </Fade>
+            <Fade in={props.selectedFsEntities.length > 0}>
+            <span>
+                <Button
+                    size="sm"
+                    variant="outline-secondary"
+                    onClick={handleDeleteClicked}
+                    disabled={props.selectedFsEntities.length < 1}
+                    className="mx-1"
+                >Delete</Button>
+                <Button
+                    size="sm"
+                    variant="outline-secondary"
+                    disabled={props.selectedFsEntities.length < 1}
+                    href={constants.url.FH_URL + "/download?ids=" + props.selectedFsEntities.map((e: FsEntity) => e.fileSystemId.toString())}
+                >Download</Button>
+            </span>
+        </Fade>
+        <NewFolder />
     </span>
     );
 }
