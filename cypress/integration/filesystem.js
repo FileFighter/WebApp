e4ab10f395968dc8b49ba5beeffb3103006c7d90
qvalentin <valentin.theodor@web.de>
2021-05-29 13:57:31 +0200

    fix tests

--- cypress/integration/filesystem.js
+++ cypress/integration/filesystem.js
@@ -1,27 +1,26 @@
 import "../support/index";
 
 describe("The filesystem page", () => {
-  beforeEach(() => {
-    cy.log("Login before every test");
+    beforeEach(() => {
+        cy.log("Login before every test");
+    });
 
-  });
+    it("changes the url when you click on a file", () => {
+        cy.loginWithUrl("/file");
+        cy.get("div.col-md-4.col-6 a").contains("Admin").click();
+        cy.url().should("include", "/admin");
+    });
 
-  it("changes the url when you click on a file", () => {
-    cy.loginWithUrl("/file");
-    cy.get("div.col-md-4.col-7").contains("Admin").click();
-    cy.url().should("include", "/admin");
-  });
+    it("goes back to the home of the filesystem", () => {
+        cy.loginWithUrl("/file/admin");
+        cy.get("div").contains("Main").click();
+        cy.url().should("not.include", "/admin");
+    });
 
-  it("goes back to the home of the filesystem", () => {
-    cy.loginWithUrl("/file/admin");
-    cy.get("div").contains("Home").click();
-    cy.url().should("not.include", "/admin");
-  });
-
-  it("shows error message when a path does not exist", () => {
-    cy.loginWithUrl("/file/fghdhjghjdfhjhdfkhg/dfghjkhghjdfhj");
-    cy.get("div").contains("Folder does not exist, or you are not allowed to see the folder.")
-  });
+    it("shows error message when a path does not exist", () => {
+        cy.loginWithUrl("/file/fghdhjghjdfhjhdfkhg/dfghjkhghjdfhj");
+        cy.get("div").contains(
+            "Folder does not exist, or you are not allowed to see the folder."
+        );
+    });
 });
-
-
