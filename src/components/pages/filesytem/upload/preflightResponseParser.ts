6adb2c695c5d7c16e3838725200de34c8ae88a5c
valentin <36446499+qvalentin@users.noreply.github.com>
2021-05-30 17:39:00 +0200

    FF-472 remove leading backslashes when needed (#185)

--- src/components/pages/filesytem/upload/preflightResponseParser.ts
+++ src/components/pages/filesytem/upload/preflightResponseParser.ts
@@ -1,19 +1,29 @@
-import { EditablePreflightEntityOrFile, PreflightEntity } from "./preflightTypes";
+import {
+    EditablePreflightEntityOrFile,
+    PreflightEntity
+} from "./preflightTypes";
 
-export const preflightResultCombine = (files: EditablePreflightEntityOrFile[], response: PreflightEntity[]) => {
+export const preflightResultCombine = (
+    files: EditablePreflightEntityOrFile[],
+    response: PreflightEntity[]
+) => {
+    console.log("[preflightResultCombine] files", files, "response", response);
+    return files.map((file: EditablePreflightEntityOrFile) => {
+        const resInfo = response.find(
+            (e) =>
+                e.path === (file.newPath ?? file.path) ||
+                "/" + e.path === (file.newPath ?? file.path)
+        );
+        // could be done better if rest returned paths with leading slashes
+        if (!resInfo) {
+            console.error("Did not found a response for the file:", file);
+        }
 
-  return files.map((file: EditablePreflightEntityOrFile) => {
-    console.log(response)
-    const resInfo = response.find((e) => (e.path === (file.newPath ?? file.path)) || ("/" + e.path === (file.newPath ?? file.path)));
-    // could be done better if rest returned paths with leading slashes
-    if (!resInfo){console.error("Did not found a response for the file:",file)}
+        file.permissionIsSufficient = resInfo?.permissionIsSufficient ?? true;
+        file.nameAlreadyInUse = resInfo?.nameAlreadyInUse ?? false;
+        file.nameIsValid = resInfo?.nameIsValid ?? true;
+        file.isFile = resInfo?.isFile ?? false;
 
-    file.permissionIsSufficient =
-      resInfo?.permissionIsSufficient ?? true;
-    file.nameAlreadyInUse = resInfo?.nameAlreadyInUse ?? false;
-    file.nameIsValid = resInfo?.nameIsValid ?? true;
-    file.isFile = resInfo?.isFile ?? true;
-
-    return file;
-  });
+        return file;
+    });
 };
