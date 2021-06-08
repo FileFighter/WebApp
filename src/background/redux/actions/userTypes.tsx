507abcacc549ac851872358988616754ef1b44c7
qvalentin <valentin.theodor@web.de>
2021-06-08 11:44:32 +0200

    remove registration from navbar when you are not admin

--- src/background/redux/actions/userTypes.tsx
+++ src/background/redux/actions/userTypes.tsx
@@ -1,15 +1,14 @@
 export const UPDATE_USER = "UPDATE_USER";
 
 export interface UserState {
-  userId: number | null;
-  username: string | null;
-  groups: number[];
+    userId: number | null;
+    username: string | null;
+    groups: string[];
 }
 
-
 export interface UpdateUser {
-  type: typeof UPDATE_USER;
-  payload: UserState;
+    type: typeof UPDATE_USER;
+    payload: UserState;
 }
 
 export type UserActionTypes = UpdateUser;
--- src/components/pages/User/Profile.tsx
+++ src/components/pages/User/Profile.tsx
@@ -1,23 +1,53 @@
-import React, {ReactElement, useState} from "react";
-import {Alert, Button, Container} from "react-bootstrap";
-import UserInformationInput, {UserInformationInputInterface} from "./UserInformationInput";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../background/redux/store";
-import {DEFAULT_ALERT_DURATION, MIN_PASSWORD_LENGTH} from "../../../background/constants";
-import {changeUserInformation, UserInformation} from "../../../background/api/userInformation";
-import {notMinStrLength} from "../../../background/methods/checkInput";
+import React, { ReactElement, useState } from "react";
+import { Alert, Button, Container } from "react-bootstrap";
+import UserInformationInput, {
+    UserInformationInputInterface
+} from "./UserInformationInput";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../background/redux/store";
+import {
+    DEFAULT_ALERT_DURATION,
+    MIN_PASSWORD_LENGTH
+} from "../../../background/constants";
+import {
+    changeUserInformation,
+    UserInformation
+} from "../../../background/api/userInformation";
+import { notMinStrLength } from "../../../background/methods/checkInput";
 import edit_svg from "../../../assets/images/icons/material.io/edit_white_24dp.svg";
-import {hashPassword} from "../../../background/methods/passwords";
-
+import { hashPassword } from "../../../background/methods/passwords";
 
 export default function Profile(): ReactElement {
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const user = useSelector((state: RootState) => state.user);
-    const [alertMessage, setAlertMessage] = useState<string>("Error 404: No Message found.");
-    const [alertVariant, setAlertColor] = useState<"primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark">("success");
+    const [alertMessage, setAlertMessage] = useState<string>(
+        "Error 404: No Message found."
+    );
+    const [alertVariant, setAlertColor] = useState<
+        | "primary"
+        | "secondary"
+        | "success"
+        | "danger"
+        | "warning"
+        | "info"
+        | "light"
+        | "dark"
+    >("success");
     const [alertVisibility, setAlertVisibility] = useState<boolean>(false);
 
-    const handleAlertVisibility = (duration: number, color: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark", message: string) => {
+    const handleAlertVisibility = (
+        duration: number,
+        color:
+            | "primary"
+            | "secondary"
+            | "success"
+            | "danger"
+            | "warning"
+            | "info"
+            | "light"
+            | "dark",
+        message: string
+    ) => {
         if (!alertVisibility) {
             setAlertMessage(message);
             setAlertColor(color);
@@ -26,31 +56,48 @@ export default function Profile(): ReactElement {
                 setAlertVisibility(false);
             }, duration);
         }
-    }
+    };
 
     function changeEditMode(): void {
-        console.log("[PROFILE] changedEditMode")
-        setIsEditing(!isEditing)
+        console.log("[PROFILE] changedEditMode");
+        setIsEditing(!isEditing);
     }
 
     const handleSubmit = async (inputUser: UserInformationInputInterface) => {
-        console.log("[PROFILE] handleSubmit")
+        console.log("[PROFILE] handleSubmit");
         let newUser: UserInformation = {
             groups: user.groups,
             userId: user.userId
-        }
+        };
         if (!inputUser.username) {
-            handleAlertVisibility(DEFAULT_ALERT_DURATION, "danger", "Error: Please choose an username.")
+            handleAlertVisibility(
+                DEFAULT_ALERT_DURATION,
+                "danger",
+                "Error: Please choose an username."
+            );
             return;
         }
         newUser["username"] = inputUser.username;
         if (inputUser.password || inputUser.passwordConfirmation) {
             if (inputUser.password !== inputUser.passwordConfirmation) {
-                handleAlertVisibility(DEFAULT_ALERT_DURATION, "danger", "Error: Password and password confirmation must match.")
+                handleAlertVisibility(
+                    DEFAULT_ALERT_DURATION,
+                    "danger",
+                    "Error: Password and password confirmation must match."
+                );
                 return;
             }
-            if (inputUser.password.match(/\d/) == null || inputUser.password.match(/[a-z]/) == null || inputUser.password.match(/[A-Z]/) == null || notMinStrLength(inputUser.password, MIN_PASSWORD_LENGTH)) {
-                handleAlertVisibility(DEFAULT_ALERT_DURATION, "danger", "Error: Please pay attention to the notes below the input fields.")
+            if (
+                inputUser.password.match(/\d/) == null ||
+                inputUser.password.match(/[a-z]/) == null ||
+                inputUser.password.match(/[A-Z]/) == null ||
+                notMinStrLength(inputUser.password, MIN_PASSWORD_LENGTH)
+            ) {
+                handleAlertVisibility(
+                    DEFAULT_ALERT_DURATION,
+                    "danger",
+                    "Error: Please pay attention to the notes below the input fields."
+                );
                 return;
             }
             newUser["password"] = await hashPassword(inputUser.password);
@@ -58,15 +105,23 @@ export default function Profile(): ReactElement {
         }
 
         await changeUserInformation(newUser)
-            .then(res => {
-                changeEditMode()
-                handleAlertVisibility(DEFAULT_ALERT_DURATION, "success", "Worked: " + (res));
+            .then((res) => {
+                changeEditMode();
+                handleAlertVisibility(
+                    DEFAULT_ALERT_DURATION,
+                    "success",
+                    "Worked: " + res
+                );
             })
-            .catch(err => {
-                console.log("Error:" + err)
-                handleAlertVisibility(DEFAULT_ALERT_DURATION, "danger", "Error: " + err)
-            })
-    }
+            .catch((err) => {
+                console.log("Error:" + err);
+                handleAlertVisibility(
+                    DEFAULT_ALERT_DURATION,
+                    "danger",
+                    "Error: " + err
+                );
+            });
+    };
 
     function EditProfile(): ReactElement {
         return (
@@ -74,51 +129,56 @@ export default function Profile(): ReactElement {
                 <UserInformationInput
                     triggerAlert={handleAlertVisibility}
                     submitFunction={handleSubmit}
-                    presets={{username: user.username ?? "", password: ""}}
+                    presets={{ username: user.username ?? "", password: "" }}
                 />
-                <Alert variant={alertVariant} onClose={() => setAlertVisibility(false)} show={alertVisibility}
-                       dismissible>
+                <Alert
+                    variant={alertVariant}
+                    onClose={() => setAlertVisibility(false)}
+                    show={alertVisibility}
+                    dismissible
+                >
                     <p>{alertMessage}</p>
                 </Alert>
             </>
-        )
+        );
     }
 
     function DisplayProfile(): ReactElement {
         return (
             <div className="profile-information-display p-0 w-100">
-                <h2 className="h3 pb-3">
-                    {user.username}
-                </h2>
+                <h2 className="h3 pb-3">{user.username}</h2>
                 <dl>
                     <dt>Username</dt>
                     <dd>{user.username}</dd>
                     <dt>Groups</dt>
-                    <dd>{user.groups?.map((value: number) => {
-                        return value + " "
-                    })}
+                    <dd>
+                        {user.groups?.map((value: string) => {
+                            return value + " ";
+                        })}
                     </dd>
                 </dl>
             </div>
-        )
+        );
     }
 
     return (
         <Container className="page-content">
             <div className="px-1 w-100 mt-1 mb-3 ">
                 <div className="w-100 title-action">
-                    <h1 className="mr-1 h4">
-                        My Profile
-                    </h1>
-                    <Button
-                        onClick={changeEditMode}
-                    >
-                        {!isEditing && <img src={edit_svg} alt={"edit icon"} className="pr-2"/>}
+                    <h1 className="mr-1 h4">My Profile</h1>
+                    <Button onClick={changeEditMode}>
+                        {!isEditing && (
+                            <img
+                                src={edit_svg}
+                                alt={"edit icon"}
+                                className="pr-2"
+                            />
+                        )}
                         {isEditing ? "Cancel" : "Edit"}
                     </Button>
                 </div>
             </div>
-            {isEditing ? <EditProfile/> : <DisplayProfile/>}
+            {isEditing ? <EditProfile /> : <DisplayProfile />}
         </Container>
     );
 }
