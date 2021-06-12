c14c355111e932ae07eab4e954b471aed3d9173a
qvalentin <valentin.theodor@web.de>
2021-06-12 11:53:24 +0200

    FF-482 dont use crypto.subtle for hashing in insecure contexts

--- src/background/methods/passwords.ts
+++ src/background/methods/passwords.ts
@@ -1,9 +1,15 @@
 //see: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 
+import sha256 from "./sha256";
+
+const salt = "FileFighterWithSomeSalt";
+
 async function hashPassword(password: string) {
-    const msgUint8 = new TextEncoder().encode(
-        password + "FileFighterWithSomeSalt"
-    ); // encode as (utf-8) Uint8Array
+    if (!crypto.subtle) {
+        return sha256(password + salt)?.toUpperCase();
+    }
+
+    const msgUint8 = new TextEncoder().encode(password + salt); // encode as (utf-8) Uint8Array
     const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
     const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
     const hashHex = hashArray
