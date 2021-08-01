6fec456d04dcf26921373113d5b37e0a22f2caf1
qvalentin <valentin.theodor@web.de>
2021-08-01 17:48:31 +0200

    add error page for 400/401 errors

---
+++ src/components/pages/errors/Error400.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+interface Props {
+    needsLogin?: boolean;
+}
+
+export default function Error400({ needsLogin }: Props) {
+    const location = useLocation();
+    const urlSearchParams = new URLSearchParams(location.search);
+    const dest = urlSearchParams.get("dest");
+    const message = urlSearchParams.get("message");
+
+    const tryAgainUrl = needsLogin
+        ? "/login?dest=" + dest
+        : decodeURIComponent(dest ?? "");
+    console.log(location);
+
+    return (
+        <Container className={"text-center"}>
+            <h2>Something went wrong!</h2>
+            <h3>The file could not be loaded</h3>
+            {message && <h4>{message}</h4>}
+            {tryAgainUrl && (
+                <p>
+                    <Link to={tryAgainUrl}>Try again</Link>
+                </p>
+            )}
+            <Link to="/">Go to Home</Link>
+        </Container>
+    );
+}
