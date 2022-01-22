6864ecb0a2676886d7e9155255f2a1569a6b4318
qvalentin <valentin.theodor@web.de>
2022-01-22 17:29:02 +0100

    (deps) update to react bootstrap v2

--- src/components/basicElements/login/Login.tsx
+++ src/components/basicElements/login/Login.tsx
@@ -146,7 +146,7 @@ export function LoginInput(props: LoginInputInterface) {
             <Row className="mt-4 justify-content-center">
                 <Col className="login-input">
                     <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="formBasicUsername">
+                        <Form.Group controlId="formBasicUsername" className="mb-3">
                             <Form.Control
                                 autoFocus
                                 placeholder="Username"
@@ -158,7 +158,7 @@ export function LoginInput(props: LoginInputInterface) {
                             />
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="formBasicPassword" className="mb-3">
                             <Form.Control
                                 type="password"
                                 placeholder="Password"
@@ -174,7 +174,7 @@ export function LoginInput(props: LoginInputInterface) {
                             </Form.Text>
                         </Form.Group>
 
-                        <Button variant="primary" type="submit" block>
+                        <Button variant="primary" type="submit" className="w-100">
                             <Spinner
                                 as="span"
                                 animation="grow"
--- src/components/pages/filesytem/upload/UploadDecisionsTableRow.tsx
+++ src/components/pages/filesytem/upload/UploadDecisionsTableRow.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback } from "react"
-import { Form } from "react-bootstrap"
+import React, {useCallback} from "react"
+import {Form} from "react-bootstrap"
 import {
     EditablePreflightEntityOrFile,
     PeflightEntiesActionTypes,
@@ -17,9 +17,9 @@ interface Props {
 }
 
 const UploadDecisionsTableRow = ({
-    preflightEntity,
-    setPreflightResultDispatch,
-}: Props) => {
+                                     preflightEntity,
+                                     setPreflightResultDispatch,
+                                 }: Props) => {
     const onNameChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
             let val = event.target.value
@@ -36,9 +36,9 @@ const UploadDecisionsTableRow = ({
         [preflightEntity.path, setPreflightResultDispatch]
     )
     const onNameInputLeaver = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            console.log("onNameInputLeaver")
-            let val = event.target.value
+        (event) => {
+            console.log("onNameInputLeaver");
+            let val = event.target.value;
 
             const change: PreflightUpdateName = {
                 type: PREFLIGHT_UPDATE_NAME,
@@ -84,7 +84,7 @@ const UploadDecisionsTableRow = ({
                             preflightEntity.overwrite
                                 ? preflightEntity.name
                                 : preflightEntity.newName ??
-                                  preflightEntity.name
+                                preflightEntity.name
                         }
                         onChange={onNameChange}
                         onBlur={onNameInputLeaver}
@@ -102,7 +102,7 @@ const UploadDecisionsTableRow = ({
                 )}
             </td>
             <td>
-                <Form.Group>
+                <Form.Group className="pl-4 mb-3">
                     <Form.Check
                         disabled={
                             !preflightEntity.nameIsValid ||
