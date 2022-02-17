2921a5a97413f9d72d019b2931010afe3a276702
Gimleux <56401687+Gimleux@users.noreply.github.com>
2022-02-17 10:36:51 +0100

    Deps/renovate snyk dependabot (#284)
    * fix: webapp_provider/Dockerfile to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-DEBIAN11-EXPAT-2331811
    - https://snyk.io/vuln/SNYK-DEBIAN11-GLIBC-2340908
    - https://snyk.io/vuln/SNYK-DEBIAN11-GLIBC-2340908
    - https://snyk.io/vuln/SNYK-DEBIAN11-GLIBC-2340922
    - https://snyk.io/vuln/SNYK-DEBIAN11-GLIBC-2340922
    
    * Bump follow-redirects from 1.14.7 to 1.14.8
    
    Bumps [follow-redirects](https://github.com/follow-redirects/follow-redirects) from 1.14.7 to 1.14.8.
    - [Release notes](https://github.com/follow-redirects/follow-redirects/releases)
    - [Commits](https://github.com/follow-redirects/follow-redirects/compare/v1.14.7...v1.14.8)
    
    ---
    updated-dependencies:
    - dependency-name: follow-redirects
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Updated Dependencies
    
    * Update Storybook deps
    
    * Updated react-scripts
    
    * edit .github yaml
    
    * edit .github yaml
    
    * remove patch uptades in package.json
    
    * set Dockerfile nginx to latest
    
    * npx sb upgrade
    
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: qvalentin <valentin.theodor@web.de>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,10 @@
 module.exports = {
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-create-react-app"
-    ],
-    typescript: {
-        reactDocgen: "none"
-    }
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  typescript: {
+    reactDocgen: "none"
+  },
+  core: {
+    builder: "webpack5"
+  }
 };
