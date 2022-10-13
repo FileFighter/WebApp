5bb60459e78c0c5bae48fc972f0d4e9d7d50abd9
Your Name <you@example.com>
2022-10-13 18:23:47 +0200

    Test fs functions

--- src/background/methods/__tests__/filesystem.test.ts
+++ src/background/methods/__tests__/filesystem.test.ts
@@ -1,4 +1,10 @@
-import { getPathWithoutName, removeTrailingBackslash } from "../filesystem"
+import { FsEntity } from "background/api/filesystemTypes"
+import {
+    getPathWithoutName,
+    removeLeadingBackslash,
+    removeTrailingBackslash,
+    isFsEntityInFolder,
+} from "../filesystem"
 
 describe("Filesystem functions", () => {
     test("Getting path without name works", () => {
@@ -18,5 +24,29 @@ describe("Filesystem functions", () => {
 
         let unchanged = removeTrailingBackslash("/hallo")
         expect(unchanged).toEqual("/hallo")
+    })
+
+    test("Removing leading backslashes", () => {
+        let result1 = removeLeadingBackslash("/somePath/")
+        expect(result1).toEqual("somePath/")
+
+        let root = removeTrailingBackslash("/")
+        expect(root).toEqual("/")
+
+        let unchanged = removeTrailingBackslash("hallo")
+        expect(unchanged).toEqual("hallo")
+    })
+
+    test("Is inode in the given folder works", () => {
+        let fsEntity = { path: "/someFile", name: "someFile" } as FsEntity
+        let result1 = isFsEntityInFolder(fsEntity, "/")
+        expect(result1).toEqual(true)
+
+        let fsEntity1 = {
+            path: "/someFolder/someFile",
+            name: "someFile",
+        } as FsEntity
+        let result2 = isFsEntityInFolder(fsEntity1, "/someFolder")
+        expect(result2).toEqual(true)
     })
 })
